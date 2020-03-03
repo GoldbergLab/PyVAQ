97023f4a42dc07ba93d96bd8e4b72bdbc3dd2030
Brian Kardon <brian.kardon@gmail.com>
2020-03-02 21:00:08 -0500

    Moved "None" to the top of the start trigger channel list

--- PyVAQ.py
+++ PyVAQ.py
@@ -1057,7 +1057,7 @@ him know. Otherwise, I had nothing to do with it.
 
         availableAudioChannels = flattenList(discoverDAQAudioChannels().values())
         availableClockChannels = flattenList(discoverDAQClockChannels().values()) + ['None']
-        availableDigitalChannels = flattenList(discoverDAQTerminals().values()) + ['None']
+        availableDigitalChannels = ['None'] + flattenList(discoverDAQTerminals().values())
         availableCamSerials = discoverCameras()
 
         params = []
