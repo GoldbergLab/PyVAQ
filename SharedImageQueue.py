8210a5388c45e188f35dddcedd975c0ce62d67b4
Brian Kardon <brian.kardon@gmail.com>
2021-11-04 11:55:26 -0400

    Added ability to handle mono/bayer images

--- SharedImageQueue.py
+++ SharedImageQueue.py
@@ -21,7 +21,7 @@ class SharedImageSender():
                 channels=3,                    # Number of color channels in images (for example, 1 for grayscale, 3 for RGB, 4 for RGBA)
                 imageDataType=ctypes.c_uint8,       # ctype of data of each pixel's channel value
                 imageBitsPerPixel=8,                # Number of bits per pixel
-                outputType='PySpin',                # Specify how to return data at receiver endpoint. Options are PySpin, numpy, PIL, rawBuffer
+                outputType='PySpin',                # Specify how to return data at receiver endpoint. Options are PySpin, numpy, PIL, bytes
                 outputCopy=True,                    # If true, the output data type references a copied buffer, rather than the original. Caution - the original buffer is not synchronized, and may be overwritten at any time!
                 fileWriter=None,                    # fileWriter must be either None or a function that takes the specified output type and writes it to a file.
                 lockForOutput=True,                # Should the shared memory buffer be locked during fileWriter call? If outputCopy=False and fileWriter is not None, it is recommended that lockForOutput=True
@@ -79,11 +79,15 @@ class SharedImageSender():
 
     def setupBuffers(self):
         # Prepare numpy buffer views inside putter process
+        if self.channels > 1:
+            frameShape = (self.height, self.width, self.channels)
+        else:
+            frameShape = (self.height, self.width)
         for k in range(self.maxBufferSize):
             # Create a series of numpy array views into that shared memory location.
             #   The numpy arrays share an underlying memory buffer with the shared memory arrays,
             #   so changing the numpy array changes the shared memory buffer (and vice versa)
-            self.npBuffers.append(np.frombuffer(self.buffers[k], dtype=ctypes.c_uint8).reshape((self.height, self.width, self.channels)))
+            self.npBuffers.append(np.frombuffer(self.buffers[k], dtype=ctypes.c_uint8).reshape(frameShape))
         self.buffersReady = True
 
     def getReceiver(self):
@@ -148,10 +152,16 @@ class SharedImageReceiver():
         self.outputCopy = outputCopy
         self.fileWriter = fileWriter
         self.imageDataType = imageDataType
+        self.channels = channels;
         self.lockForOutput = lockForOutput
         self.metadataQueue = metadataQueue
         self.verbose = verbose
         self.nextID = 0
+
+        if self.channels > 1:
+            self.frameShape = (self.height, self.width, self.channels)
+        else:
+            self.frameShape = (self.height, self.width)
 
     def getNextID(self):
         nextID = self.nextID
@@ -164,29 +174,38 @@ class SharedImageReceiver():
     def prepareOutput(self, data):
         # As specified, copies, converts, and/or writes data to a file
         if self.outputType == 'PySpin':
-            imarray = np.frombuffer(data, dtype=self.imageDataType).reshape((self.height, self.width, 3))
+            imarray = np.frombuffer(data, dtype=self.imageDataType).reshape(self.frameShape)
             output = PySpin.Image.Create(self.width, self.height, self.offsetX, self.offsetY, self.pixelFormat, imarray)
             if self.verbose: print("Got image: ", imarray[0:5, 0, 0])
             if self.outputCopy:
                 pass # Do we need to copy this? Does PySpin.Image.Create already copy the buffer? Dunno, need to test to find out
         elif self.outputType == 'PIL':
-            if self.outputCopy:
-                output = Image.frombytes("RGB", (self.width, self.height), data, "raw", 'RGB')
+            if self.channels == 1:
+                format = "L"
+            elif self.channels == 3:
+                format = "RGB"
             else:
-                output = Image.frombuffer("RGB", (self.width, self.height), data, "raw", 'RGB')
+                raise ValueError('Invalid channel count: {c}. Must be 1 or 3.'.format(c=self.channels))
+            if self.outputCopy:
+                output = Image.frombytes(format, (self.width, self.height), data, "raw", format, 0, 1)
+            else:
+                output = Image.frombuffer(format, (self.width, self.height), data, "raw", format, 0, 1)
             if self.verbose: print("Got image: ", np.array(output)[0:5, 0, 0])
         elif self.outputType == 'numpy':
-            output = np.frombuffer(data, dtype=self.imageDataType).reshape((self.height, self.width, 3))
+            output = np.frombuffer(data, dtype=self.imageDataType).reshape(self.frameShape)
             if self.outputCopy:
                 output = np.copy(output)
-        elif self.outputType == 'rawBuffer':
+        elif self.outputType == 'bytes':
             output = data
             if self.outputCopy:
                 output = output[:]
+        else:
+            raise KeyError('Unrecognized output type: ', self.outputType)
 
         # If given, apply the fileWriter function to the output
         if self.fileWriter:
             self.fileWriter(output)
+            print('Applied filewriter!')
         return output
 
     def get(self, includeMetadata=False):
@@ -209,7 +228,6 @@ class SharedImageReceiver():
             output = self.prepareOutput(data)
 
         metadata = self.metadataQueue.get(block=False)
-
 
         if includeMetadata:
             return output, metadata
