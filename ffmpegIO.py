f21ed7bf76d21974662c1f7a8d47115489c88cc2
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-04-25 13:56:33 -0400

    Added ffmpegIO.ffmpegPipedVideoDiscarder

--- ffmpegIO.py
+++ ffmpegIO.py
@@ -284,6 +284,23 @@ class ffmpegVideoWriter():
         if self.verbose >= 2:
             print('Closed pipe to ffmpeg')
 
+class ffmpegPipedVideoDiscarder(ffmpegPipedVideoWriter):
+    def __init__(self, *args, **kwargs):
+        """Construct an ffmpegPipedVideoDiscarder, which reads and discards data
+
+            *args (list): Arguments to pass to ffmpegPipedVideoWriter parent class
+            **kwargs (dict): Arguments to pass to ffmpegPipedVideoWriter parent class
+        """
+        super().__init__('', '')
+
+def dispatchFFMPEG(self, *args):
+    """Open an ffmpegNull process
+    """
+
+    if self.verbose >= 2: print('dispatching ffmpegNull for discarding data:')
+
+    self.ffmpegProc = subprocess.Popen('python ffmpegNull.py', stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 class ffmpegPipedVideoWriter(ffmpegVideoWriter):
     def __init__(self, filename, pipePath, pipeDiscardQueue=None, numFrames=None, **kwargs):
         """Construct an ffmpegPipedVideoWriter.
---
+++ ffmpegNull.py
@@ -0,0 +1,7 @@
+import sys
+
+# Read data from stream, and discard it
+data = sys.stdin.buffer.read()
+
+# Exit
+sys.exit(0)
