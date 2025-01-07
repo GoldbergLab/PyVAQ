7633cda738089a27fa49e39e158aa571c7cb9ecb
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2025-01-07 11:47:10 -0500

    Various bugfixes, create demo
    When module is run directly, a ffplayer demo plays

--- ffplayViewer.py
+++ ffplayViewer.py
@@ -1,16 +1,33 @@
 import subprocess
 import numpy as np
 import time
+import cv2
+import traceback
 
 
-class ffplayViewer:
-    def __init__(self, maxFrameRate, windowTitle='ffplayViewer'):
+class ffplayer:
+    def __init__(self, maxFrameRate, windowTitle='ffplayer', pixelFormat='rgb24'):
         self.width = None
         self.height = None
-        self.maxFrameRate = None
+        self.maxFrameRate = maxFrameRate
         self.windowTitle = windowTitle
         self.ffplay_cmd = None
         self.ffplay_proc = None
+        self.pixelFormat = pixelFormat
+
+    def blank(self):
+        # Display a blank frame indicating lack of image data
+        if self.width is not None and self.height is not None:
+            msg = 'no image data'
+            blankFrame = np.zeros([self.height, self.width, 3], dtype='uint8')
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 255, 255)
+            thickness = 2
+            (textWidth, textHeight), baseline = cv2.getTextSize(msg, font, fontScale, thickness)
+            origin = ((self.width - textWidth)//2, (self.height + textHeight)//2)
+            blankFrame = cv2.putText(blankFrame, msg, origin, font, fontScale, color, thickness)
+            self.showFrame(blankFrame)
 
     def initialize(self, width, height):
         """Set up the ffplay process.
@@ -25,23 +42,33 @@ class ffplayViewer:
         """
         self.width = width
         self.height = height
+        print('Initializing ffplayer with:')
+        print('    width', self.width)
+        print('    height', self.height)
+        print('    pixelFormat', self.pixelFormat)
+        print('    maxFrameRate*2', self.maxFrameRate*2)
+        print('    windowTitle', self.windowTitle)
 
         self.ffplay_cmd = [
             'ffplay',
             '-f', 'rawvideo',
-            '-pixel_format', 'rgb24',
+            '-pixel_format', self.pixelFormat,
             '-video_size', f'{self.width}x{self.height}',
             '-framerate', str(self.maxFrameRate*2),
             '-i', '-',
             # '-vf', f'drawtext=fontfile=\'C\\:/Windows/Fonts/Arial.ttf\':text={text}:fontcolor={color}:fontsize={fontsize}:x={textX}:y={textY}',  # ensure scaling (optional)
             '-window_title', self.windowTitle,             # give a window title
             '-hide_banner',
-            '-loglevel', 'quiet',
+            # '-loglevel', 'verbose',
             '-nostats'
             ]
 
         # Start ffplay in a subprocess with a pipe for stdin
-        self.ffplay_proc = subprocess.Popen(ffplay_cmd, stdin=subprocess.PIPE)
+        self.ffplay_proc = subprocess.Popen(
+            self.ffplay_cmd,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
 
     def showFrame(self, frame):
         """Display a frame on the screen.
@@ -52,12 +79,46 @@ class ffplayViewer:
         """
         if self.ffplay_proc is None:
             self.initialize(frame.shape[1], frame.shape[0])
-        proc.stdin.write(frame.tobytes())
-        proc.stdin.flush()
-
-    def close(self):
+        try:
+            self.ffplay_proc.stdin.write(frame.tobytes())
+            self.ffplay_proc.stdin.flush()
+        except BrokenPipeError:
+            print('ffplayer: Broken pipe')
+            print(traceback.format_exc())
+            self.close()
+        except OSError:
+            print('ffplay: other error')
+            print(traceback.format_exc())
+            self.close()
+    def close(self, timeout=2):
         """Close the ffplay process
         """
-        if self.proc.poll() is None:
-            self.proc.stdin.close()
-            self.proc.wait()
+        if self.ffplay_proc:
+            if self.ffplay_proc.stdin:
+                self.ffplay_proc.stdin.close()
+            output, err = self.ffplay_proc.communicate(timeout=timeout)
+        else:
+            output = ''
+            err = ''
+        self.ffplay_proc = None
+        return output, err
+
+if __name__ == "__main__":
+    viewer = ffplayer(60, 'ffplayViewer.ffplayer example')
+    try:
+        k = 0
+        while True:
+            image = np.random.randint(0, 255, size=(1000, 1000, 3), dtype='uint8')
+            if k < 50:
+                viewer.showFrame(image)
+            else:
+                viewer.blank()
+            time.sleep(1/30)
+            k = k + 1
+            k = k % 100
+    except KeyboardInterrupt:
+        output, err = viewer.close()
+        print('output')
+        print(output.decode('utf-8'))
+        print('err')
+        print(err.decode('utf-8'))
